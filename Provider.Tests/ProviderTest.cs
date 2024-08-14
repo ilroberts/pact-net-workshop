53d7b43d28a998537ce0acf5b9195b219d2f9774
iwan.roberts <iwan.roberts@defra.onmicrosoft.com>
2024-08-14 10:00:50 +0100

    working (but failing) provider test

---
+++ Provider.Tests/ProviderTest.cs
@@ -0,0 +1,38 @@
+using PactNet.Verifier;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Provider;
+
+namespace tests
+{
+  public class ProductTest
+  {
+    private string _pactServiceUri = "http://127.0.0.1:9001";
+
+    [Fact]
+    public void EnsureProviderApiHonoursPactWithConsumer()
+    {
+      var config = new PactVerifierConfig
+      {
+        // NOTE: We default to using a ConsoleOutput, however xUnit 2 does not capture the console output,
+        // so a custom outputter is required.
+        Outputters = [
+            new PactNet.Infrastructure.Outputters.ConsoleOutput(),
+                ]
+      };
+      var args = Array.Empty<string>();
+      using var _webHost = WebHost.CreateDefaultBuilder(args)
+          .UseStartup<Startup>()
+          .UseUrls(_pactServiceUri)
+          .Build();
+      _webHost.Start();
+
+      using var verifier = new PactVerifier(new PactVerifierConfig());
+      var pactFolder = new DirectoryInfo(Path.Join(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "Consumer.Tests", "pacts"));
+      verifier.ServiceProvider("ProductService", new Uri(_pactServiceUri))
+      .WithDirectorySource(pactFolder)
+      .Verify();
+    }
+  }
+}
+
