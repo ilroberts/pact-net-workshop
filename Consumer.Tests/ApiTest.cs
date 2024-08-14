5356e09c68e2e8f3e4297f06cddee56a3cb5e20f
iwan.roberts <iwan.roberts@defra.onmicrosoft.com>
2024-08-14 14:18:16 +0100

    tweaked the test to get a failing provider test for returning no data

--- Consumer.Tests/ApiTest.cs
+++ Consumer.Tests/ApiTest.cs
@@ -9,70 +9,108 @@ using Newtonsoft.Json.Serialization;
 
 namespace Consumer.Tests
 {
-    public class ApiTest
+  public class ApiTest
+  {
+    private readonly IPactBuilderV3 pact;
+    private readonly ApiClient ApiClient;
+    private readonly int port = 9000;
+    private readonly List<object> products;
+
+    public ApiTest(ITestOutputHelper output)
     {
-        private readonly IPactBuilderV3 pact;
-        private readonly ApiClient ApiClient;
-        private readonly int port = 9000;
-        private readonly List<object> products;
+      products =
+      [
+          new { id = 9, type = "CREDIT_CARD", name = "GEM Visa", version = "v2" },
+          new { id = 10, type = "CREDIT_CARD", name = "28 Degrees", version = "v1" }
+      ];
 
-        public ApiTest(ITestOutputHelper output)
+      var Config = new PactConfig
+      {
+        Outputters =
+        [
+          new ConsoleOutput()
+        ],
+        DefaultJsonSettings = new JsonSerializerSettings
         {
-            products =
-            [
-                new { id = 9, type = "CREDIT_CARD", name = "GEM Visa", version = "v2" },
-                new { id = 10, type = "CREDIT_CARD", name = "28 Degrees", version = "v1" }
-            ];
-
-            var Config = new PactConfig
-            {
-              Outputters =
-              [
-                new ConsoleOutput()
-              ],
-              DefaultJsonSettings = new JsonSerializerSettings
-              {
-                ContractResolver = new CamelCasePropertyNamesContractResolver()
-              }
-            };            //pact = Pact.V3("ApiClient", "ProductService", Config).UsingNativeBackend(port);
+          ContractResolver = new CamelCasePropertyNamesContractResolver()
+        }
+      };
       this.pact = Pact.V3("ApiClient", "ProductService", Config).WithHttpInteractions(port);
       ApiClient = new ApiClient(new Uri($"http://localhost:{port}"));
     }
 
-        [Fact]
-        public async void GetAllProducts()
-        {
-            // Arange
-            pact.UponReceiving("A valid request for all products")
-                    .Given("There is data")
-                    .WithRequest(HttpMethod.Get, "/api/products")
-                .WillRespond()
-                    .WithStatus(HttpStatusCode.OK)
-                    .WithHeader("Content-Type", "application/json; charset=utf-8")
-                    .WithJsonBody(new TypeMatcher(products));
+    [Fact]
+    public async void GetAllProducts()
+    {
+      // Arange
+      pact.UponReceiving("A valid request for all products")
+              .Given("There is data")
+              .WithRequest(HttpMethod.Get, "/api/products")
+          .WillRespond()
+              .WithStatus(HttpStatusCode.OK)
+              .WithHeader("Content-Type", "application/json; charset=utf-8")
+              .WithJsonBody(new TypeMatcher(products));
 
-            await pact.VerifyAsync(async ctx => {
-                var response = await ApiClient.GetAllProducts();
-                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            });
-        }
-
-        [Fact]
-        public async void GetProduct()
-        {
-            // Arange
-            pact.UponReceiving("A valid request for a product")
-                    .Given("There is data")
-                    .WithRequest(HttpMethod.Get, "/api/products/10")
-                .WillRespond()
-                    .WithStatus(HttpStatusCode.OK)
-                    .WithHeader("Content-Type", "application/json; charset=utf-8")
-                    .WithJsonBody(new TypeMatcher(products[1]));
-
-            await pact.VerifyAsync(async ctx => {
-                var response = await ApiClient.GetProduct(10);
-                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            });
-        }
+      await pact.VerifyAsync(async ctx =>
+      {
+        var response = await ApiClient.GetAllProducts();
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+      });
     }
+
+    [Fact]
+    public async void GetProduct()
+    {
+      pact.UponReceiving("A valid request for a product")
+              .Given("There is data")
+              .WithRequest(HttpMethod.Get, "/api/products/10")
+          .WillRespond()
+              .WithStatus(HttpStatusCode.OK)
+              .WithHeader("Content-Type", "application/json; charset=utf-8")
+              .WithJsonBody(new TypeMatcher(products[1]));
+
+      await pact.VerifyAsync(async ctx =>
+      {
+        var response = await ApiClient.GetProduct(10);
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+      });
+    }
+
+    [Fact]
+    public async void NoProductExists()
+    {
+      pact.UponReceiving("A valid request for all products")
+              .Given("no product exists")
+              .WithRequest(HttpMethod.Get, "/api/products")
+          .WillRespond()
+              .WithStatus(HttpStatusCode.OK)
+              .WithHeader("Content-Type", "application/json; charset=utf-8")
+              .WithJsonBody(new List<object>());
+
+      await pact.VerifyAsync(async ctx =>
+      {
+        var response = await ApiClient.GetAllProducts();
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var body = await response.Content.ReadAsStringAsync();
+        Console.WriteLine($"received body: {body}");
+        Assert.Equal("[]", body);
+      });
+    }
+
+    [Fact]
+    public async void ProductDoesNotExist()
+    {
+      pact.UponReceiving("A valid request for a product")
+              .Given("product with ID 11 does not exist")
+              .WithRequest(HttpMethod.Get, "/api/products/11")
+          .WillRespond()
+              .WithStatus(HttpStatusCode.NotFound);
+
+      await pact.VerifyAsync(async ctx =>
+      {
+        var response = await ApiClient.GetProduct(11);
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+      });
+    }
+  }
 }
